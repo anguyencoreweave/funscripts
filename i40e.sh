582db3b92b9b06fcee7db07c97674495f8e57087
anguyencoreweave <142817027+anguyencoreweave@users.noreply.github.com>
2023-12-22 11:13:39 -0500

    Update i40e.sh

--- i40e.sh
+++ i40e.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "---------------------------------------------"
-echo "$(date): Starting i40e driver installation script - this script is used for setting up dual port Intel XL710 Green color NICs that are freshly installed in A40s."
-echo "---------------------------------------------"
+echo "------------------------------------------------------------------------"
+echo "$(date): Starting i40e driver update script for Intel XL710 NIC (green color)"
+echo "------------------------------------------------------------------------"
+echo "This script is designed to update the i40e driver and is mainly used on A40 nodes"
+echo "when installing a new Intel XL710 (green color) dual-port NIC card."
+echo "------------------------------------------------------------------------"
 
 echo "$(date): Downloading i40e driver..."
 sudo wget https://downloadmirror.intel.com/786085/i40e-2.23.17.tar.gz
@@ -22,6 +25,6 @@ sudo modprobe i40e
 echo "$(date): Displaying information about the i40e module..."
 sudo modinfo i40e
 
-echo "---------------------------------------------"
+echo "------------------------------------------------------------------------"
 echo "$(date): Installation and setup completed successfully."
-echo "---------------------------------------------"
+echo "------------------------------------------------------------------------"
